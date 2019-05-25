dd666ddefd2899d79686484cf422745e1141cca8
Joffref <51022808+Joffref@users.noreply.github.com>
2019-05-25 15:06:05 +0200

    Update GitHub_handler.py

--- GitHub_handler.py
+++ GitHub_handler.py
@@ -1,14 +1,14 @@
-import os 
+from os import system 
 
 def github_push(documents, subtitles):
     for document in documents:
         for subtitle in subtitles:
-            os.system('git add ' + document)
-            os.system('git commit -m  "{}"'.format(subtitle))
+            system('git add ' + document)
+            system('git commit -m  "{}"'.format(subtitle))
 
 def github_fetch(names):
     for name in names:
-        os.system('git fetch ' + name)
+        system('git fetch ' + name)
     
 
         
